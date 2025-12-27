0060badbe722addfb6d65f215fe30b79d71eb74f
Puneet Garg <puneetgarg80@gmail.com>
2025-12-27 15:49:36 +0000

    feat: Update existing game level definitions and add new practice levels.

--- client/constants.ts
+++ client/constants.ts
@@ -17,26 +17,26 @@ const rect = (w: number, h: number) => {
 const ARENA_LEVELS: LevelData[] = [
   {
     id: 'main-challenge',
-    name: 'Gym: Level 4',
+    name: 'Gym: Level 6',
     par: 1,
-    targetCells: rect(8, 8), // 8x8 Square (64)
+    targetCells: rect(6, 5),   // 6x5 Square (3 6)
     initialShape: [
-      ...rect(8, 7), // Top 7 rows
-      // 8th row
-      ...rect(1, 1).map(c => ({ x: c.x, y: c.y + 7 })),
-      ...rect(5, 1).map(c => ({ x: c.x + 2, y: c.y + 7 })),
-      // 9th row
-      ...rect(1, 1).map(c => ({ x: c.x + 2, y: c.y + 8 })),
-      ...rect(1, 1).map(c => ({ x: c.x + 6, y: c.y + 8 })),
+      ...rect(6, 4), // Top 4 rows
+      // 5th row
+      ...rect(1, 1).map(c => ({ x: c.x, y: c.y + 4 })),
+      ...rect(3, 1).map(c => ({ x: c.x + 2, y: c.y + 4 })),
+      // 6th row
+      ...rect(1, 1).map(c => ({ x: c.x + 2, y: c.y + 5 })),
+      ...rect(1, 1).map(c => ({ x: c.x + 4, y: c.y + 5 })),
     ],
-    targetOffset: { x: 2, y: 10 } // Bottom
+    targetOffset: { x: 2, y: 8 } // Bottom
   }
 ];
 
 const GYM_LEVELS: LevelData[] = [
   {
-    id: 'practice-level-0',
-    name: 'Gym: Level 0',
+    id: 'practice-level-1',
+    name: 'Gym: Level 1',
     par: 1,
     targetCells: rect(5, 5), // 5x5 Square (25)
     initialShape: [
@@ -44,55 +44,72 @@ const GYM_LEVELS: LevelData[] = [
       // 3rd row
       ...rect(5, 1).map(c => ({ x: c.x - 1, y: c.y + 2 })),
       // 4,5th rows
-      ...rect(5, 2).map(c => ({ x: c.x - 1, y: c.y + 3 }))
+      ...rect(5, 2).map(c => ({ x: c.x, y: c.y + 3 }))
     ],
-    targetOffset: { x: 2, y: 10 } // Bottom
-  },
-  {
-    id: 'practice-level-1',
-    name: 'Gym: Level 1',
-    par: 1,
-    targetCells: rect(8, 8), // 8x8 Square (64)
-    initialShape: [
-      ...rect(8, 3), // Top 3 rows
-      // 4th row
-      ...rect(7, 1).map(c => ({ x: c.x, y: c.y + 3 })),
-      // 5,6,7,8th rows
-      ...rect(8, 4).map(c => ({ x: c.x, y: c.y + 4 })),
-      // 9th row
-      ...rect(1, 1).map(c => ({ x: c.x + 1, y: c.y + 8 })),
-    ],
-    targetOffset: { x: 2, y: 10 } // Bottom
+    targetOffset: { x: 2, y: 8 } // Bottom
   },
   {
     id: 'practice-level-2',
     name: 'Gym: Level 2',
     par: 1,
-    targetCells: rect(8, 8), // 8x8 Square (64)
+    targetCells: rect(5, 5), // 5x5 Square (25)
     initialShape: [
-      ...rect(8, 7), // Top 7 rows
-      // 8th row
-      ...rect(1, 1).map(c => ({ x: c.x, y: c.y + 7 })),
-      ...rect(6, 1).map(c => ({ x: c.x + 2, y: c.y + 7 })),
-      // 9th row
-      ...rect(1, 1).map(c => ({ x: c.x + 6, y: c.y + 8 })),
+      ...rect(5, 1), // Top 1 row
+      // 2nd row
+      ...rect(6, 1).map(c => ({ x: c.x - 1, y: c.y + 1 })),
+      // 3rd row
+      ...rect(5, 1).map(c => ({ x: c.x, y: c.y + 2 })),
+      // 4th rows
+      ...rect(4, 1).map(c => ({ x: c.x, y: c.y + 3 })),
+      // 5th rows
+      ...rect(5, 1).map(c => ({ x: c.x, y: c.y + 4 }))
     ],
-    targetOffset: { x: 2, y: 10 } // Bottom
+    targetOffset: { x: 2, y: 8 } // Bottom
   },
   {
     id: 'practice-level-3',
     name: 'Gym: Level 3',
     par: 1,
-    targetCells: rect(8, 8), // 8x8 Square (64)
+    targetCells: rect(4, 4), // 4x4 Square (16)
     initialShape: [
-      ...rect(8, 7), // Top 7 rows
-      // 8th row
-      ...rect(1, 1).map(c => ({ x: c.x, y: c.y + 7 })),
-      ...rect(7, 1).map(c => ({ x: c.x, y: c.y + 7 })),
-      // 9th row
-      ...rect(1, 1).map(c => ({ x: c.x + 2, y: c.y + 8 })),
+      ...rect(4, 2), // Top 2 rows
+      // 3rd row
+      ...rect(3, 1).map(c => ({ x: c.x, y: c.y + 2 })),
+      // 4th row
+      ...rect(4, 1).map(c => ({ x: c.x, y: c.y + 3 })),
+      // 5th row
+      ...rect(1, 1).map(c => ({ x: c.x + 1, y: c.y + 4 })),
     ],
-    targetOffset: { x: 2, y: 11 } // Bottom
+    targetOffset: { x: 2, y: 8 } // Bottom
+  },
+  {
+    id: 'practice-level-4',
+    name: 'Gym: Level 4',
+    par: 1,
+    targetCells: rect(5, 5), // 5x5 Square (25)
+    initialShape: [
+      ...rect(5, 4), // Top 4 rows
+      // 5th row
+      ...rect(1, 1).map(c => ({ x: c.x, y: c.y + 4 })),
+      ...rect(3, 1).map(c => ({ x: c.x + 2, y: c.y + 4 })),
+      // 6th row
+      ...rect(1, 1).map(c => ({ x: c.x + 3, y: c.y + 5 })),
+    ],
+    targetOffset: { x: 2, y: 8 } // Bottom
+  },
+  {
+    id: 'practice-level-5',
+    name: 'Gym: Level 5',
+    par: 1,
+    targetCells: rect(4, 4), // 4x4 Square (16)
+    initialShape: [
+      ...rect(4, 3), // Top 3 rows
+      // 4th row
+      ...rect(3, 1).map(c => ({ x: c.x, y: c.y + 3 })),
+      // 5th row
+      ...rect(1, 1).map(c => ({ x: c.x + 1, y: c.y + 4 })),
+    ],
+    targetOffset: { x: 2, y: 8 } // Bottom
   }
 ];
 
